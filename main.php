683a26bf0b340c7322441061f8042389076c17de
P1ratRuleZZZ <p1ratrulezzz@gmail.com>
2015-03-29 13:03:16 +0300

    Fixed supported formats list

--- main.php
+++ main.php
@@ -408,7 +408,7 @@ if ( !class_exists("MP3j_Main") ) { class MP3j_Main	{
 	 *  Returns an array of tracks and false in case of errors or if couldn't find any mp3s.
 	 */
 	 public function grab_remote_folder_mp3s($scheme, $folder) {
-	 	  $permited_exts = $this->theSettings['audioFormats'];
+	 	  $permited_exts = $this->getAudioFormats();
 		  // Build correct url.
 		  $url = strtolower($scheme) . '://' . trim($folder, '/');
 		  // Set fixed timeouts in order to prevent long page loadings.
